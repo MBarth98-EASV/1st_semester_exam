1ce2c948c7d45663dae4fab91c2c5eedee1bb3a1
PhilEZad <90150224+PhilEZad@users.noreply.github.com>
2022-01-05 13:37:09 +0100

    init database access

---
+++ src/main/java/dal/EASVDatabase.java
@@ -0,0 +1,21 @@
+package dal;
+
+import com.microsoft.sqlserver.jdbc.SQLServerDataSource;
+
+public class EASVDatabase {
+
+    private SQLServerDataSource dbaccess;
+
+    public EASVDatabase()
+    {
+        dbaccess = new SQLServerDataSource();
+        dbaccess.setServerName("10.176.111.31");
+        dbaccess.setDatabaseName("CSe21A_29_MovieManager");
+        dbaccess.setUser("CSe21A_29");
+        dbaccess.setPassword("itsikkerhed");
+        dbaccess.setPortNumber(1433);
+    }
+
+
+
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,7 @@ module easv.app {
     requires javafx.fxml;
     requires javafx.media;
     requires javafx.controls;
+    requires com.microsoft.sqlserver.jdbc;
 
     opens easv.app to javafx.fxml;
     exports easv.app;
