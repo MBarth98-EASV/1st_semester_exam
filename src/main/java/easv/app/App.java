db2761840c4f9ace7d35e3cfbdb132c3e5fc4b10
sandbxk <90904706+sandbxk@users.noreply.github.com>
2022-01-11 12:47:42 +0100

    Update App.java

--- src/main/java/easv/app/App.java
+++ src/main/java/easv/app/App.java
@@ -15,7 +15,7 @@ public class App extends Application
         FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("MovieManager.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1000, 800);
         stage.setTitle("Popkernel Time");
-        stage.setMinHeight(788);
+        stage.setMinHeight(800);
         stage.setMinWidth(866);
         stage.setScene(scene);
         stage.show();
